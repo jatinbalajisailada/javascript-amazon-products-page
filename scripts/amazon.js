c77543469f51a342f8357e0185a6a26c0c1a6580
jatinbalajisailada <sailadajatinbalaji@gmail.com>
2025-09-28 01:18:41 +0530

    cart features upgraded

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -74,7 +74,14 @@ document.querySelectorAll('.js-add-to-cart').forEach(button => {
           productId: productId,
           quantity: 1
         });
-      };
-    console.log(cart);
+    };
+
+    let cartQuantity = 0;
+    cart.forEach(item => {
+      cartQuantity += item.quantity;
+    });
+
+    document.querySelector('.js-cart-quantity').innerText = cartQuantity;
+    button.innerText = 'Added';
   });
 });
